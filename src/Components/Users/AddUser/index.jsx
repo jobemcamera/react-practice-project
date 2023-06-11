c6d5baa3a0d722d965efaa5b90349968d95d9a92
jobe-lumni <jobecamera@gmail.com>
2023-06-11 09:00:27 -0300

    criando os componentes Card e AddUser

---
+++ src/Components/Users/AddUser/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Card from '../../UI/Card';
+
+const AddUser = () => {
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type="text" />
+        <label htmlFor='age'>Age</label>
+        <input id='age' type="number" />
+        <button type='submit'>Add User</button>
+      </form>
+    </Card>
+  )
+}
+
+export default AddUser
